0799a197e27a2467bbc03b2724ce7426156d3b3d
yasminsheikh3125 <yasminsheikh1250@gmail.com>
2026-04-02 12:46:03 +0530

    Dropped low influence columns and updated final.csv

--- src/feature_engineering.py
+++ src/feature_engineering.py
@@ -10,15 +10,7 @@ df = pd.read_csv("data/processed/cleaned_churn.csv")
 # Dropping gender column (not needed for model)
 df = df.drop(columns=["gender"])
 
-# Converting country into numerical values
-df = pd.get_dummies(df, columns=["country"], drop_first=True)
 
-# Converting encoded columns to integer type
-df[['country_Germany','country_Spain']] = \
-df[['country_Germany','country_Spain']].astype(int)
-
-# Created a new feature to show relationship between balance and salary
-df['balance_salary_ratio'] = df['balance'] / (df['estimated_salary'] + 1)
 
 # Dropping unnecessary columns
 final_df = df.drop(columns=[
