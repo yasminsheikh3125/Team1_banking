f2ff0df1ff79fdba353905ada008231f1b459e71
yasminsheikh3125 <yasminsheikh1250@gmail.com>
2026-04-07 15:33:19 +0530

    Feature engineering updated

--- src/feature_engineering.py
+++ src/feature_engineering.py
@@ -7,10 +7,17 @@ import pandas as pd
 # Loading cleaned dataset
 df = pd.read_csv("../data/processed/cleaned_churn.csv")
 
+#one hot encoding for country
+df = pd.get_dummies(df, columns=['country','gender'],drop_first=True)
+
+#convert boolean to int
+df[['country_Germany', 'country_Spain','gender_Male']] = df[['country_Germany', 'country_Spain','gender_Male']].astype(int)
+
 # Dropping gender column (not needed for model)
-df = df.drop(columns=["gender"])
+#df = df.drop(columns=["gender"])
 
-
+male_count = df['gender_Male'].sum()
+female_count = len(df) - male_count
 
 # Dropping unnecessary columns
 final_df = df.drop(columns=[
@@ -22,6 +29,8 @@ final_df = df.drop(columns=[
 # Saving processed dataset for model training
 final_df.to_csv("../data/processed/final.csv", index=False)
 
+print(male_count)
+print(female_count)
 print("Feature engineering and data preparation completed!")
 
 
